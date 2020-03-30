aca919edd7ef8a2f1784b0355ffaad7f5f6ce678
AnhTuanIT <phamanhtuan24031999@gmail.com>
2020-03-30 15:57:37 +0700

    difference of 2

---
+++ Kyu6/differenceof2.js
@@ -0,0 +1,20 @@
+const arr = [1, 2, 3, 4];
+
+function twosDifference(input) {
+    let newArr = input.sort((a, b) => a - b);
+    let mapArr = Array.from(newArr, (e) => [e, 0]);
+    let outputArr = Array.from(mapArr, (e, index) => {
+        let len = newArr.length;
+        for (let i = 0; i < len; i++) {
+            if (e[0] + 2 === newArr[i]) {
+                e.pop();
+                e.push(newArr[i]);
+                newArr.splice(i, 1);
+                return e;
+            }
+        }
+    });
+    return outputArr.filter((val) => val !== undefined);
+}
+
+console.log(twosDifference(arr));
