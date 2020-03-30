dbb7ff7a0bfe538d3b691dab106b7bdefc46ed3c
AnhTuanIT <phamanhtuan24031999@gmail.com>
2020-03-30 23:26:25 +0700

    morezerosthanones

---
+++ Kyu6/morezerosthanones.js
@@ -0,0 +1,25 @@
+let str = null;
+
+function handleToBinary(str) {
+    return str.split('').map((e) => e.charCodeAt(0).toString(2));
+}
+
+function handleCompare(str) {
+    return str.match(/[0]/g).length > 3;
+}
+
+function moreZeros(s) {
+    if (s == null) {
+        return [];
+    }
+    let outputArr = handleToBinary(s).filter((e) => handleCompare(e)).map(e => {
+        return String.fromCharCode(parseInt(e, 2));
+    }).join('').match(/[^1-9]/g);
+    // return outputArr;
+    return [...new Set(outputArr)];
+}
+
+// console.log(handleToBinary(str));
+console.log(moreZeros(str));
+
+// https://www.codewars.com/kata/5d41e16d8bad42002208fe1a/train/javascript
