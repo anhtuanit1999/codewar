c3b8dc648babf1ff150832cc1568e9fe7a3f56ec
AnhTuanIT <46421325+anhtuanit1999@users.noreply.github.com>
2020-03-31 20:55:40 +0700

    match them chuoi 1 la duoc

--- Kyu6/morezerosthanones.js
+++ Kyu6/morezerosthanones.js
@@ -4,14 +4,15 @@ function handleToBinary(str) {
     return str.split('').map((e) => e.charCodeAt(0).toString(2));
 }
 
+function handleToBinary(str) {
+    return str.split('').map((e) => e.charCodeAt(0).toString(2));
+}
+
 function handleCompare(str) {
-    return str.match(/[0]/g).length > 3;
+    return str.match(/[0]/g) ? str.match(/[0]/g).length > 3 : str.match(/[1]/g).length <= 3;
 }
 
 function moreZeros(s) {
-    if (s == null) {
-        return [];
-    }
     let outputArr = handleToBinary(s).filter((e) => handleCompare(e)).map(e => {
         return String.fromCharCode(parseInt(e, 2));
     }).join('').match(/[^1-9]/g);
