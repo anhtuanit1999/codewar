089b903359972f5a02e31060329cafe47ec46f4a
AnhTuanIT <phamanhtuan24031999@gmail.com>
2020-04-02 18:39:40 +0700

    What's wrong with these identifiers?

---
+++ Kyu8/Anidentifierissimplyaname.js
@@ -0,0 +1,25 @@
+let Person = {
+    firstName: "John",
+    secondName: "Doe",
+    emailAddress: "john.doe@email.com",
+    maleFemale: "M"
+};
+
+// TODO: Add your tests here
+// Starting from Node 10.x, [Mocha](https://mochajs.org) is used instead of our custom test framework.
+// [Codewars' assertion methods](https://github.com/Codewars/codewars.com/wiki/Codewars-JavaScript-Test-Framework)
+// are still available for now.
+//
+// For new tests, using [Chai](https://chaijs.com/) is recommended.
+// You can use it by requiring:
+//     const assert = require("chai").assert;
+// If the failure output for deep equality is truncated, `chai.config.truncateThreshold` can be adjusted.
+
+describe("Solution", function() {
+    it("should test for something", function() {
+        // Test.assertEquals(1 + 1, 2);
+        // assert.strictEqual(1 + 1, 2);
+    });
+});
+
+// https: //www.codewars.com/kata/56bb01de0e8b29de50000b19/train/javascript
