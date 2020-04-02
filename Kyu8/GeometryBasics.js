b7dcffb5d980b48f1a4fd6be74784af4b2bf0d2e
AnhTuanIT <phamanhtuan24031999@gmail.com>
2020-04-02 19:06:40 +0700

    Geometry Basics: Distance between points in 2D

--- Kyu8/GeometryBasics.js
+++ Kyu8/GeometryBasics.js
@@ -1,5 +1,6 @@
 function distanceBetweenPoints(a, b) {
     return Math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2);
+    // return Math.hypot(a.x - b.x, a.y - b.y);
 }
 
 // https://www.codewars.com/kata/58dced7b702b805b200000be/train/javascript
